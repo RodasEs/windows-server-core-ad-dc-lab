d6cb857db34bc0642c74a402ee270c1fa6d44576
Rodas Es <RodasEs@github.com>
2026-02-07 15:34:28 -0500

    format edit

--- commands/00-verify-pre.cmd
+++ commands/00-verify-pre.cmd
@@ -7,11 +7,16 @@ whoami
 
 echo.
 
-echo [2] System role + domain fields: systeminfo | findstr /B /C: "OS Configuration" /C:"Domain"
+echo [2] System role + domain fields: 
+
+systeminfo | findstr /B /C: "OS Configuration" /C:"Domain"
 
 echo.
+
 echo [3] Attempt DC search for lab.local (this command is expected to fail pre-promotion): 
+
 nltest /dsgetdc:lab.local
 
 echo.
+
 echo === END ===
